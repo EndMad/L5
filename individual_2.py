d068bc1ce1058b746ffc52505f2073ee5253342f
EndMad <70961155+EndMad@users.noreply.github.com>
2021-01-08 01:57:52 +0300

    Add files via upload

---
+++ individual_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    lst = list(map(int, input('Введите список ').split()))
+    print(sum(not bool(i) for i in lst))
+    print(sum(lst[lst.index(min(lst)) + 1:]))
+    lst.sort(key=lambda x: abs(x))
+    print(lst)

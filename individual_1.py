90e3b2864deed021fb37f522990299528e69d7e5
EndMad <70961155+EndMad@users.noreply.github.com>
2021-01-27 10:37:54 +0300

    Update individual_1.py

--- individual_1.py
+++ individual_1.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Ввести список А из 10 элементов, найти сумму элементов,
+# меньших по модулю 3 и кратных 9,
+# их количество и вывести результаты на экран.
+
 from random import randint
-A = [randint(-100,10) for i in range(10)]
+A = [randint(-100, 10) for i in range(10)]
 print(*A)
 print(*[i for i in A if not i % 9 and i < 3])
 print(sum((1 for i in A if not i % 9 and i < 3)))
 print(sum((i for i in A if not i % 9 and i < 3)))
+

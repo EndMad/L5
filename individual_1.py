a332e7c76f297cb736380c7f0b6ec314460ba4b6
EndMad <70961155+EndMad@users.noreply.github.com>
2021-01-27 10:45:18 +0300

    Update individual_1.py

--- individual_1.py
+++ individual_1.py
@@ -5,10 +5,10 @@
 # меньших по модулю 3 и кратных 9,
 # их количество и вывести результаты на экран.
 
-from random import randint
-A = [randint(-100, 10) for i in range(10)]
-print(*A)
-print(*[i for i in A if not i % 9 and i < 3])
-print(sum((1 for i in A if not i % 9 and i < 3)))
-print(sum((i for i in A if not i % 9 and i < 3)))
-
+if __name__ == '__main__':
+    from random import randint
+    A = [randint(-100, 10) for i in range(10)]
+    print(*A)
+    print(*[i for i in A if not i % 9 and i < 3])
+    print(sum((1 for i in A if not i % 9 and i < 3)))
+    print(sum((i for i in A if not i % 9 and i < 3)))
